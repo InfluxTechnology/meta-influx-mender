4db2293f1d7c7e4874d06aa0438e1d08fa564b00
Andon Nikolov <superflay123@gmail.com>
2026-05-18 16:38:55 +0300

    single-rootfs

---
+++ recipes-influx/rootfs-single/files/inf-resizefs.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+systemctl --no-reload disable resizefs.service
+exit 0
+
+mv /data/mender /opt/influx/
+
+# Locate mmc device with a boot0 partition and extract device path
+dev=`ls /dev/mmcblk*boot*`
+dev=($dev)
+dev=${dev[0]}
+devpath=${dev%boot*}
+
+# Extract partition's numbers
+PART_NUM_3=$(parted $devpath -ms unit s p | tail -n 2 | head -n 1 | cut -f 1 -d:)
+PART_NUM_4=$(parted $devpath -ms unit s p | tail -n 1 | cut -f 1 -d:)
+
+# Extract partition's start/end sectors
+PART_START_3=$(parted $devpath -ms unit s p | grep "^${PART_NUM_3}" | cut -f 2 -d:)
+PART_END_4=$(parted $devpath -ms unit s p | grep "^${PART_NUM_4}" | cut -f 3 -d:)
+
+# remove last character
+PART_START_3=${PART_START_3%?}
+PART_END_4=${PART_END_4%?}
+
+echo $PART_NUM_4 $PART_START_3 $PART_END_4
+
+# Run fdisk in scripted mode
+fdisk "$devpath" <<EOF
+d
+$PART_NUM_3
+d
+$PART_NUM_4
+n
+p
+$PART_NUM_4
+$PART_START_3
+$PART_END_4
+w
+EOF
+
+# Reload partition table
+partprobe "$devpath" || true
+
+# Remove the service. Should only be run once
+systemctl --no-reload disable resizefs.service
+
+mv /opt/influx/mender /data
+
+#reboot
