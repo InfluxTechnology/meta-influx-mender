df23e57a8772fe0a22d44b52225abf5e55c3421a
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2026-05-20 09:23:51 +0300

    Update influx-setup-single.sh

--- scripts/influx-setup-single.sh
+++ scripts/influx-setup-single.sh
@@ -4,3 +4,4 @@
 cat ../sources/meta-influx-mender/templates/local.conf.single >> conf/local.conf
 
 patch -Np1 -r - ../sources/meta-influx-mender/recipes-influx/influx-files/files/Download_Enter_00 < ../sources/meta-influx-mender/templates/Download_Enter.patch
+patch -Np1 -r - ../sources/meta-influx-mender/recipes-mender/mender-client/mender_%.bbappend < ../sources/meta-influx-mender/templates/mender-append.patch
